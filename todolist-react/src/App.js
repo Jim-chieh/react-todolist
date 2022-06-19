953e56a967d619f364410934a3c60fd37ca8b2e7
Jay <chriese0112@gmail.com>
2022-06-19 17:43:08 +0800

    保存初始

---
+++ todolist-react/src/App.js
@@ -0,0 +1,13 @@
+import {Menu} from 'semantic-ui-react'
+
+
+function App (){
+  return (
+    <Menu fluid widths={16}>
+      Todo List
+    </Menu>
+  )
+}
+
+
+export default App
---
+++ todolist-react/src/index.js
@@ -0,0 +1,13 @@
+import App from './App'
+import 'semantic-ui-css/semantic.min.css'
+
+import {createRoot} from 'react-dom/client'
+
+
+const rootElement = document.getElementById('root')
+
+const root = createRoot(rootElement)
+
+root.render(
+  <App />
+)
